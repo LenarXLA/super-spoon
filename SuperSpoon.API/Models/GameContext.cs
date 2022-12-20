a4d7746361a3e75f8c752732be38b389897debdd
LenarXLA <leonruscraft@gmail.com>
2022-12-20 14:31:18 +0500

    init: init new project

---
+++ SuperSpoon.API/Models/GameContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperSpoon.API.Models
+{
+    public class GameContext : DbContext
+    {
+        public GameContext (DbContextOptions<GameContext> options) : base(options) { }
+
+        public DbSet<GameItem> GameItems { get; set; } = null!;
+    }
+}
---
+++ SuperSpoon.API/Models/GameItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperSpoon.API.Models
+{
+    public class GameItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
